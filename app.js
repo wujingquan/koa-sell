8442ae696a2152823c0c1dcd771769488f58984c
wujingquan <i@wujingquan.com>
2018-10-23 00:48:24 +0800

    change file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
-const static = require('koa-static');
 
 // 模拟数据
 const mockData = require('./data.json');
@@ -11,9 +10,6 @@ const ratings = mockData.ratings;
 
 const app = new Koa();
 const router = new Router();
-
-// 静态文件路径
-const staticPath = path.join(__dirname, './static');
 
 router.get('/api/seller', (ctx, next) => {
   ctx.body = { errno: 0, data: seller };
@@ -27,8 +23,7 @@ router.get('/api/ratings', (ctx, next) => {
   ctx.body = { errno: 0, data: ratings };
 });
 
-// 加载中间件
-app.use(static(staticPath));
+// // 加载中间件
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, '127.0.0.1');
